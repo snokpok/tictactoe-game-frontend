e8d83612ef4f5374e46ee4fb0b512c1755ceae00
Vincent Vu <vinceny.fe@gmail.com>
2021-11-03 11:55:30 -0700

    validating createGame form

--- src/components/Board/CreateGameForm.tsx
+++ src/components/Board/CreateGameForm.tsx
@@ -2,16 +2,21 @@ import { useFormik } from "formik";
 import React from "react";
 import { GameStateContext } from "../../common/context/gameState.context";
 import { ServerInteractionAPI } from "../../common/ServerInteractionAPI";
+import * as Yup from "yup";
 
 function CreateGameForm() {
   const { gameState, setGameState } = React.useContext(GameStateContext);
+  const createGameSchema = Yup.object().shape({
+    p0: Yup.string().required("Please enter player 0's name"),
+    p1: Yup.string().required("Please enter player 1's name"),
+  });
 
   const formik = useFormik({
     initialValues: {
-      gameId: "",
       p0: "",
       p1: "",
     },
+    validationSchema: createGameSchema,
     onSubmit: async (values) => {
       const players = [values.p0, values.p1];
       const createGameData = await ServerInteractionAPI.createGame(players);
@@ -30,6 +35,11 @@ function CreateGameForm() {
         e.preventDefault();
         formik.handleSubmit();
       }}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        width: 300,
+      }}
     >
       <input
         id="p0"
@@ -43,6 +53,8 @@ function CreateGameForm() {
         name="p1"
         onChange={formik.handleChange}
       />
+      {formik.errors.p0 && <div>{formik.errors.p0}</div>}
+      {formik.errors.p1 && <div>{formik.errors.p1}</div>}
       <input type="submit" />
     </form>
   );
